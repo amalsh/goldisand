88201ece51fbf66de46506711715e29148ac2591
Amal Shashika <amalshashika@gmail.com>
2019-04-05 03:03:48 +0530

    invoice splitter

--- app/code/local/Goldie/Partnersales/Model/Observer.php
+++ app/code/local/Goldie/Partnersales/Model/Observer.php
@@ -1,19 +1,20 @@
 <?php
 
 
-class Observer
+class Goldie_Partnersales_Model_Observer
 {
 
     /***
      * Set partner cookies when controller before load
      */
-    public function setPartnerCookie($observer)
+    public function setPartnerCookie(Varien_Event_Observer $observer)
     {
 
         $partnerId = null;
         $partnerId = Mage::app()->getRequest()->getParam('partner');
         //if only parameter exists set cookies
-        if ($partnerId) {
+        if ($partnerId != null) {
+
             Mage::helper('goldie_partnersales')->setPartnerCookie($partnerId);
         }
 
@@ -21,12 +22,14 @@ class Observer
 
     /***
      * Split invoces and shipments
+     *
      * @param $observer
      */
     public function partnerInvoiceCreator($observer)
     {   //if only partner exists we need to split invoices
         if (Mage::helper('goldie_partnersales')->hasPartnerExists()) {
-
+            $order = $observer->getEvent()->getOrder();
+            $splitter = new Goldie_Partnersales_Model_Ordersplitter($order);
         }
     }
 }
